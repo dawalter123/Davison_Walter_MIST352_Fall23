2d02c4045386cb302151226c1375126cf250662d
dawalter <dawalter@localhost>
2023-10-10 23:21:52 -0400

    HW #1

--- Homeworks/HW1/src/HW1A.java
+++ Homeworks/HW1/src/HW1A.java
@@ -1,18 +1,26 @@
-/**
- * 
- */
+/*
+		i. Author: Davison Walter 
+		ii. Class: MIST352-Fall23
+		iii. HW #1
+		iv.This program will print out the words and value: 
+		     Hi
+		     Sum: 15.5
+		*/
+//Name should be the homework name. 
+public class HW1A {                      
+public static void main(String[] args) {
 
-/**
- * 
- */
-public class HW1A {
+System.out.println("Hi"); 
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+int num1 = 10;
+double num2 = 5.5;
+//Line of code that was below was not needed. 
 
-	}
+//Should be 'double sum' instead of 'int sum' 
+double sum = num1 + num2;
+System.out.println("Sum:" + sum);
 
 }
+
+}
+
---
+++ Homeworks/HW1/src/HW1B.java
@@ -0,0 +1,16 @@
+public class HW1B {
+ 
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hi"); 
+
+		int num1 = 10;
+		double num2 = 5.5;
+		double sum = num1 + num2;
+
+		System.out.println("Sum:" + sum);
+}
+	
+	
+}
