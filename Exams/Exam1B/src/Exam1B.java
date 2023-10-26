a8a97c758c448ab24c0da589376e6883e52d2cbd
dawalter <dawalter@localhost>
2023-10-25 20:32:37 -0400

    Exam1B

--- Exams/Exam1B/src/Exam1B.java
+++ Exams/Exam1B/src/Exam1B.java
@@ -1,10 +1,13 @@
-/**
- * 
- */
+ /*Davison_Walter 
+  * 9/29/23
+  * Exam 1 Part B 
+  * This program greets the user, asks them for input. 
+  * The program prints the length of the input. 
+  * It asks the user for a character from their input. 
+  * The program finds the location of the character within the input. 
+  */
 
-/**
- * 
- */
+import java.util.Scanner;
 public class Exam1B {
 
 	/**
@@ -12,7 +15,21 @@ public class Exam1B {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+System.out.println("Hello!");
+Scanner var = new Scanner(System.in);
+System.out.println("Enter username");
 
+String userName = var.nextLine();
+System.out.println("Input is: " + userName);
+System.out.print("Input length is: ");
+System.out.println(userName.length());
+System.out.println("Enter a character in your input");
+String username = var.nextLine();
+System.out.println("Location is: ");
+
+
+
+		
 	}
 
 }
